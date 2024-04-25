a9f84b19e46e1f02a0699e8109a6defb7ae4e555
Blue <bilulanlv168@gmail.com>
2024-04-25 17:44:24 +0800

    自动添加配置文件

--- src/CacheManager.php
+++ src/CacheManager.php
@@ -45,10 +45,10 @@ class CacheManager extends Manager implements CacheInterface
     public function getConfig(string $name = null, $default = null)
     {
         if (!is_null($name)) {
-            return config('plugin.bilulanlv.app' . $name, $default);
+            return config('plugin.bilulanlv.think-cache.app' . $name, $default);
         }
 
-        return config('plugin.bilulanlv.app');
+        return config('plugin.bilulanlv.think-cache.app');
     }
 
     /**
